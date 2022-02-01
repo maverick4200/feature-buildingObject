827be7ca3c9b43bb30c28951a77b0bf44b72983e
maverick4200 <joshnava2@gmail.com>
2022-02-01 17:26:11 -0600

    building object. added json package.

---
+++ GuitarMaker.ts
@@ -0,0 +1,11 @@
+class GuitarMaker {
+    private guitarBodyShape:string;
+
+    constructor(petalColor:string) {
+        this.guitarBodyShape = this.guitarBodyShape
+    }
+
+disp():void {
+    console.log("The guitar's body shape is : "+this.guitarBodyShape)
+    }
+}
