690cc1f5cf36a6c83dac872731705b03f886f30f
maverick4200 <joshnava2@gmail.com>
2022-02-09 10:41:00 -0600

    added new class. fixed code.

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import {GuitarMaker} from "./GuitarMaker";
 import {ElectricGuitar} from "./ElectricGuitar"
 import {AcousticGuitar} from "./AcousticGuitar"
+import { Keytar } from "./Keytar";
 
 let newGuitar = new GuitarMaker("StratGuitar");
 console.log(newGuitar.disp());
 let newEletricGuitar = new ElectricGuitar("electricGuitar");
-let newAcousticGuitar = new AcousticGuitar("acousticGuitar");
+let newAcousticGuitar = new Keytar("acousticGuitar");
 
-console.log(newAcousticGuitar.acousticGuitar());
+console.log(newAcousticGuitar.dispGuitarType());
 console.log(newAcousticGuitar.disp());
-console.log( newEletricGuitar.electricGuitar());
+console.log( newEletricGuitar.dispGuitarType());
 console.log(newEletricGuitar.disp());
 
 
